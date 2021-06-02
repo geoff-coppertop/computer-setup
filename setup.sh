3d46c8b577107cd60585440a42aa8f568bf7c109
Geoffrey Thomas <geoff.coppertop@gmail.com>
2021-06-02 08:11:21 -0600

    Add script to run the setup process
    Add a script which will setup and run the playbook.
    
    Change-Id: Idee57aeb757e90117bbca2d56f4316b8a8e1a8f9

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+    brew upgrade
+fi
+
+brew install python@3.9
+brew install git
+
+initdir=$pwd
+workdir="/tmp/computer-setup"
+if [ -d "$workdir" ]; then
+  rm -Rf $workdir
+fi
+mkdir -p $workdir && cd $workdir
+
+git clone https://github.com/geoff-coppertop/computer-setup.git .
+
+python3.9 -m pip install --upgrade pip
+pip3 install virtualenv
+
+venvdir="./.venv"
+virtualenv $venvdir
+source "$venvdir/bin/activate"
+
+pip install -r ./requirements.txt
+ansible-galaxy install -r ./requirements.yml
+
+ansible-playbook ./playbooks/setup.yml -K
+
+deactivate
+
+cd $initdir
+rm -rf $workdir
